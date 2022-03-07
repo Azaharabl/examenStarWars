026ab705ccfd23dfb1313a44db7f34ef4d1b4890
Azahara Blanco Rodriguez <blancaluna92>
2022-03-07 13:52:52 +0100

    hecho tda, naves, y main

---
+++ src/com/azahara/ControladorDeGuerra.java
@@ -0,0 +1,4 @@
+package com.azahara;
+
+public class ControladorDeGuerra {
+}
--- src/com/azahara/Main.java
+++ src/com/azahara/Main.java
@@ -1,8 +1,43 @@
 package com.azahara;
 
+import com.azahara.Naves.Corellian;
+import com.azahara.Naves.Nave;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+
+        ControladorDeGuerra cont = new ControladorDeGuerra();
+
+        int TAMAÑO = cont.pedirTamañoEspacio();
+        int TIEMPO = cont.pedirTiempoSimulacion();
+        int temporizador = 0;
+        boolean guerraFin = false;
+
+        cont.iniciarEspacioDeGuerra(TAMAÑO);
+
+        cont.crearNavesDeGuerra();
+
+        cont.rellenarEspacio();
+
+        do {
+
+            Nave n= cont.elegirNave();
+
+            cont.lucha(n);
+
+            if(temporizador%5){cont.rellenarEspacio();};
+
+            guerrafin = cont.comprobarLuchaSigue();
+
+            temporizador++;
+
+
+        }while(!guerraFin || temporizador<TIEMPO );
+
+
+        cont.mensajeFin();
+
+
     }
 }
